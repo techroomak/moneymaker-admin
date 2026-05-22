bacdf90e4891f2bcf2a5b5d9f13c978b95ba5459
techroomak <techroomak.info@gmail.com>
2026-05-22 08:40:10 +0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,64 @@
+import { auth }
+from "./firebase.js";
 
+import {
+signInWithEmailAndPassword
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+window.loginAdmin =
+async()=>{
+
+const email =
+document.getElementById(
+"email"
+).value;
+
+const password =
+document.getElementById(
+"password"
+).value;
+
+const status =
+document.getElementById(
+"loginStatus"
+);
+
+if(!email || !password){
+
+status.innerText =
+"Fill all fields";
+
+return;
+
+}
+
+try{
+
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+status.style.color =
+"#22c55e";
+
+status.innerText =
+"Login Success";
+
+setTimeout(()=>{
+
+window.location.href =
+"dashboard.html";
+
+},1000);
+
+}catch(error){
+
+status.innerText =
+"Invalid Email or Password";
+
+}
+
+};
