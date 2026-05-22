925b084176088c5c7637ff24cd7b438c9bec6269
techroomak <techroomak.info@gmail.com>
2026-05-22 08:39:48 +0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,36 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+
+apiKey:"YOUR_API_KEY",
+
+authDomain:
+"moneymaker-bd.firebaseapp.com",
+
+projectId:
+"moneymaker-bd",
+
+storageBucket:
+"moneymaker-bd.appspot.com",
+
+messagingSenderId:"",
+
+appId:""
+
+};
+
+const app =
+initializeApp(firebaseConfig);
+
+export const auth =
+getAuth(app);
+
+export const db =
+getFirestore(app);
